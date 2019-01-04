e8d1738551203043f38b2ca2fd0959d858e6722d
ll <ll@DESKTOP-PR5G7BK>
2019-01-04 21:22:00 +0800

    local3

--- PhoneInfoServerIIS/HTTPHandle.vb
+++ PhoneInfoServerIIS/HTTPHandle.vb
@@ -23,6 +23,7 @@ Imports System.IO.Compression
 
 Public Class HTTPHandle
     Private test As String = "abc"
+    Private test1 As String = "test1"
     Structure TtLTECellInfo '铁塔工参
         Dim carrier As String
         Dim province As String 'PROVINCE
